b852e9883b92d69706441a2ca2746240e3051a68
sayakongit <83216382+sayakongit@users.noreply.github.com>
2023-01-12 20:30:27 +0530

    final commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
-# Create the parser
+
 parser = argparse.ArgumentParser()
-# Add an argument
+
 parser.add_argument('--addAccess', type=str, choices=['READ', 'WRITE'])
 parser.add_argument('--addResource', type=str)
 parser.add_argument('--addAccessOnResource', type=str, nargs=2)
@@ -11,10 +11,9 @@ parser.add_argument('--addAccessOnResourceToRole', type=str, nargs=3)
 parser.add_argument('--addRoleToUser', type=str, nargs=2)
 parser.add_argument('--checkAccess', type=str, nargs=3)
 
-# Parse the argument
 arg1 = parser.parse_args()
 resources = {}
-roles = []
+roles = {}
 users = {}
 
 
@@ -23,7 +22,7 @@ if arg1.addAccess:
     
 if arg1.addResource:
     resources[arg1.addResource] = []
-    print(arg1.addResource, "has been added a resource")
+    print(arg1.addResource, "has been added as a resource")
     
 if arg1.addAccessOnResource:
     access, resource = arg1.addAccessOnResource
@@ -32,11 +31,15 @@ if arg1.addAccessOnResource:
     
 if arg1.addAccessOnResourceToRole:
     access, resource, role = arg1.addAccessOnResourceToRole
-    
+    temp = {
+        'access': access,
+        'resource': resource
+    }
+    roles[role].append(temp)
     print(resource, "has been given", access, 'access to', role)
     
 if arg1.addRole:
-    roles.append(arg1.addRole)
+    roles[arg1.addRole] = []
     print(arg1.addRole, "role has been created")
     
 if arg1.addUser:
@@ -50,4 +53,8 @@ if arg1.addRoleToUser:
     
 if arg1.checkAccess:
     user, resource, access = arg1.checkAccess
-    print("Yes")
+    temp_user = users[user]
+    if temp_user['resource'] == resource and temp_user['access'] == access:
+        print('Yes')
+    
+    print("No")
